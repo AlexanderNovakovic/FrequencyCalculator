8bd016d19d4c16b4ef3a2818f49006f0f6655b62
AlexanderStorM <salerion1@gmail.com>
2018-12-11 22:23:54 +0100

    Add D3T2 with unit tests
    Add D3T2 with unit tests

--- Day3Tasks/TasksDayThree.cs
+++ Day3Tasks/TasksDayThree.cs
@@ -24,19 +24,42 @@
             return overlappingSurface;
         }
 
+        public static int FindNonOverlappingClaimId(string[] fileText)
+        {
+            int[][] totalSurface = FillTotalSurfaceWithZeros(1000);
+
+            MakeAllClaims(fileText, totalSurface);
+
+            foreach (string line in fileText)
+            {
+                Claim claim = ReadClaimFromString(line);
+
+                if (!IsOverlapping(claim, totalSurface))
+                    return claim.Id;
+            }
+
+            return 0;            
+        }
+
+        public static bool IsOverlapping(Claim claim, int[][] totalSurface)
+        {
+            for (int i = claim.Rectangle.TopLeftX; i < claim.Rectangle.TopLeftX + claim.Rectangle.Width; i++)
+            {
+                for(int j = claim.Rectangle.TopLeftY; j < claim.Rectangle.TopLeftY + claim.Rectangle.Height; j++)
+                {
+                    if(totalSurface[i][j] > 1)
+                        return true;
+                }
+            }
+
+            return false;
+        }
+
         public static void MakeAllClaims(string[] input, int[][] surface)
         {
             foreach (string line in input)
             {
-                int id = ReturnIdFrom(line);
-                int topLeftX = ReturnTopLeftXFrom(line);
-                int topLeftY = ReturnTopLeftYFrom(line);
-                int width = ReturnWidthFrom(line);
-                int height = ReturnHeightFrom(line);
-
-                Rectangle rectangle = new Rectangle(topLeftX, topLeftY, width, height);
-
-                Claim claim = new Claim(id, rectangle);
+                Claim claim = ReadClaimFromString(line);
 
                 MakeClaim(surface, claim);
             }
@@ -67,6 +90,21 @@
             }
 
             return totalSurface;
+        }
+
+        public static Claim ReadClaimFromString(string line)
+        {
+            int id = ReturnIdFrom(line);
+            int topLeftX = ReturnTopLeftXFrom(line);
+            int topLeftY = ReturnTopLeftYFrom(line);
+            int width = ReturnWidthFrom(line);
+            int height = ReturnHeightFrom(line);
+
+            Rectangle rectangle = new Rectangle(topLeftX, topLeftY, width, height);
+
+            Claim claim = new Claim(id, rectangle);
+
+            return claim;
         }
 
         public static int ReturnIdFrom(string line)
--- UnitTests/Day3Tests.cs
+++ UnitTests/Day3Tests.cs
@@ -14,11 +14,26 @@ namespace UnitTests
         }
 
         [Fact]
-        public void CalculateChecksumTestTwo()
+        public void CalculateOverlappingSurfaceTestTwo()
         {
             string[] input = ReadStringArrayFromFile("day3_input.txt");
 
             Assert.Equal(101781, CalculateOverlappingSurface(input));
+        }
+
+        [Theory]
+        [InlineData(new string[] { "#1 @ 1,3: 4x4", "#2 @ 3,1: 4x4", "#3 @ 5,5: 2x2" }, 3)]
+        public void FindNonOverlappingClaimIdTest(string[] input, int expected)
+        {
+            Assert.Equal(expected, FindNonOverlappingClaimId(input));
+        }
+
+        [Fact]
+        public void FindNonOverlappingClaimIdTestTwo()
+        {
+            string[] input = ReadStringArrayFromFile("day3_input.txt");
+
+            Assert.Equal(909, FindNonOverlappingClaimId(input));
         }
     }
 }
